51a45afdd1b5d00bb8f01d3a2647df38f72bc655
patrickleyba <pleyba4@cnm.edu>
2020-01-09 16:33:13 -0700

    Almost done, need to fix errors. Fixed line 34, 43 with console log in order to resolve item from not getting deleted. setDeletebutton id was not inside let itemhtml.

--- script.js
+++ script.js
@@ -10,27 +10,48 @@ function initShoppingList() {
 	});
 }
 function handleItemForm (event, formRef) {
-	if(event.preventDefault()) {
+	if(event.preventDefault) {
 		 event.preventDefault();
 	}
 
-	let itemHtml = addItemToShoppingList();
+	addItemToShoppingList();
 
 	return false;
 }
 
-function addItemToShoppingList() {
-	let itemName = document.getElementById("item-name");
-	let itemAmount = document.getElementById("item-amount")
-
-	let itemHtml = createListItemHtml(itemName.value, itemAmount.value);
-	console.log("Item HTML: ", itemHtml);
-	let itemListRef = document.getElementById("shopping-list")
-	itemListRef.insertAdjacentHTML("afterend", itemHtml)
+function getRandomInt(min, max) {
+	min= Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function createListItemHtml(itemName, itemAmount) {
-	return `<li>
-					${itemName.value} - ${itemAmount.value}
+function addItemToShoppingList() {
+	let itemName = document.getElementById("item-name");
+	let itemAmount = document.getElementById("item-amount");
+	let id = getRandomInt(0, 10000000);
+
+	let itemHtml = createListItemHtml(itemName.value, itemAmount.value, id);
+	console.log("Item HTML: ", itemHtml);
+	let itemListRef = document.getElementById("shopping-list");
+	itemListRef.insertAdjacentHTML("afterend", itemHtml)
+	setDeleteButtonEvent(id)
+}
+
+function setDeleteButtonEvent(id) {
+	let deleteButton = document.getElementById("button" + id);
+	deleteButton.addEventListener("click", () => {
+		removeListItem(id)
+	});
+}
+
+function createListItemHtml(itemName, itemAmount, id) {
+	return `<li id="item${id}">
+					${itemName} - ${itemAmount}
+					<button id="button${id}" type="button">Delete Item</button>
 			 </li>`;
+}
+
+function removeListItem(id) {
+	let listItem = document.getElementById("item"+id);
+	listItem.parentNode.removeChild(listItem);
 }
